eea7c2a23ed6b2a8f7f27c2fe1ae342700d8b5d2
ja99 <janis.boersig@gmx.de>
2021-06-30 19:24:15 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# SphericalCoordinates_Unity
+# SphericalCoordinates for Unity
  
+Implements Vector3 to Spherical Coordinates and Spherical Coordinates to Vector3 Conversion
---
+++ SphericalCoordinates.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphericalCoordinates : MonoBehaviour
+{
+   
+    /// <summary>converts Carteisian Coordinates to Spherical
+    ///  see: https://www.omnicalculator.com/math/spherical-coordinates</summary>
+    public static SphericalCoordinate CarteisianToSpherical(Vector3 direction)
+    {
+        float r = direction.magnitude;
+        float thetha = Mathf.Acos(direction.y / r);
+        float phi = Mathf.Atan2(direction.z, direction.x);
+
+        return new SphericalCoordinate(r, thetha * Mathf.Rad2Deg, phi * Mathf.Rad2Deg);
+    }
+
+    /// <summary>converts Spherical Coordinates to Catreisian
+    ///  see: https://www.omnicalculator.com/math/spherical-coordinates</summary>
+    public static Vector3 SphericalToCarteisian(SphericalCoordinate sCoordinate)
+    {
+        float z = sCoordinate.r * Mathf.Sin(sCoordinate.thetha * Mathf.Deg2Rad) * Mathf.Cos(sCoordinate.phi * Mathf.Deg2Rad);
+        float x = sCoordinate.r * Mathf.Sin(sCoordinate.thetha * Mathf.Deg2Rad) * Mathf.Sin(sCoordinate.phi * Mathf.Deg2Rad);
+        float y = sCoordinate.r * Mathf.Cos(sCoordinate.thetha * Mathf.Deg2Rad);
+
+        return new Vector3(x, y, z);
+    }
+}
+
+/// <summary>Spherical Coordinate implementation.
+///  see: https://www.omnicalculator.com/math/spherical-coordinates</summary>
+public struct SphericalCoordinate
+{
+    public float r;
+    public float thetha;
+    public float phi;
+
+    public SphericalCoordinate(float r, float thetha, float phi)
+    {
+        this.r = r;
+        this.thetha = thetha;
+        this.phi = phi;
+    }
+
+    public override string ToString()
+    {
+        string s = "(";
+        s += "r: " + this.r;
+        s += ", thetha: " + this.thetha;
+        s += ", phi: " + this.phi + ")";
+        return s;
+    }
+}
